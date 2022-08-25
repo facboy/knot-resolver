webfutureiorepo/knotdns

manager: datamodel: test types rendering

manager/tests/unit/datamodel/templates/test_types_render.py
@@ -3,8 +3,10 @@ from typing import Any
 import pytest
 from jinja2 import Template
 
-from knot_resolver_manager.datamodel.types import EscQuotesStr
+from knot_resolver_manager.datamodel.types import EscQuotesString, RawString
 from knot_resolver_manager.utils.modelling import SchemaNode
+
+str_template = Template('"{{ string }}"')
 
 
 @pytest.mark.parametrize(
@@ -15,15 +17,36 @@ from knot_resolver_manager.utils.modelling import SchemaNode
         ('"double quotes"', r"\"double quotes\""),
         ("'single quotes'", r"\'single quotes\'"),
         # fmt: off
-        ('\"double quotes\"', r"\"double quotes\""),
-        ("\'single quotes\'", r"\'single quotes\'"),
+        ('\"double quotes\"', r'\"double quotes\"'),
+        ("\'single quotes\'", r'\'single quotes\''),
         # fmt: on
     ],
 )
-def test_escaped_quotes_string(val: Any, exp: str):
+def test_esc_quotes_string(val: Any, exp: str):
     class TestSchema(SchemaNode):
-        escaped: EscQuotesStr
+        escaped: EscQuotesString
 
     d = TestSchema({"escaped": val})
-    tmpl = Template('"{{ string }}"')
-    assert tmpl.render(string=d.escaped) == f'"{exp}"'
+    assert str_template.render(string=d.escaped) == f'"{exp}"'
+
+
+@pytest.mark.parametrize(
+    "val,exp",
+    [
+        ("string", "string"),
+        (2000, "2000"),
+        ('\n\t"', r"\n\t\""),
+        ('"double quotes"', r"\"double quotes\""),
+        ("'single quotes'", r"\'single quotes\'"),
+        # fmt: off
+        ('\"double quotes\"', r'\"double quotes\"'),
+        ("\'single quotes\'", r'\'single quotes\''),
+        # fmt: on
+    ],
+)
+def test_raw_string(val: Any, exp: str):
+    class TestSchema(SchemaNode):
+        pattern: RawString
+
+    d = TestSchema({"pattern": val})
+    assert str_template.render(string=d.pattern) == f'"{exp}"'

facboy/knot-resolver

manager/tests/unit/datamodel/templates/test_types_render.py
@@ -3,8 +3,10 @@ from typing import Any
 import pytest
 from jinja2 import Template
 
-from knot_resolver_manager.datamodel.types import EscQuotesStr
+from knot_resolver_manager.datamodel.types import EscQuotesString, RawString
 from knot_resolver_manager.utils.modeling import BaseSchema
+
+str_template = Template('"{{ string }}"')
 
 
 @pytest.mark.parametrize(
@@ -15,15 +17,36 @@ from knot_resolver_manager.utils.modeling import BaseSchema
         ('"double quotes"', r"\"double quotes\""),
         ("'single quotes'", r"\'single quotes\'"),
         # fmt: off
-        ('\"double quotes\"', r"\"double quotes\""),
-        ("\'single quotes\'", r"\'single quotes\'"),
+        ('\"double quotes\"', r'\"double quotes\"'),
+        ("\'single quotes\'", r'\'single quotes\''),
         # fmt: on
     ],
 )
 def test_escaped_quotes_string(val: Any, exp: str):
     class TestSchema(BaseSchema):
-        escaped: EscQuotesStr
+        escaped: EscQuotesString
 
     d = TestSchema({"escaped": val})
-    tmpl = Template('"{{ string }}"')
-    assert tmpl.render(string=d.escaped) == f'"{exp}"'
+    assert str_template.render(string=d.escaped) == f'"{exp}"'
+
+
+@pytest.mark.parametrize(
+    "val,exp",
+    [
+        ("string", "string"),
+        (2000, "2000"),
+        ('\n\t"', r"\n\t\""),
+        ('"double quotes"', r"\"double quotes\""),
+        ("'single quotes'", r"\'single quotes\'"),
+        # fmt: off
+        ('\"double quotes\"', r'\"double quotes\"'),
+        ("\'single quotes\'", r'\'single quotes\''),
+        # fmt: on
+    ],
+)
+def test_raw_string(val: Any, exp: str):
+    class TestSchema(BaseSchema):
+        pattern: RawString
+
+    d = TestSchema({"pattern": val})
+    assert str_template.render(string=d.pattern) == f'"{exp}"'