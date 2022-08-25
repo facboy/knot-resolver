webfutureiorepo/knotdns

manager: datamodel: base type to escape chars

manager/knot_resolver_manager/datamodel/types/base_types.py
@@ -1,5 +1,5 @@
 import re
-from typing import Any, Dict, Pattern, Type
+from typing import Any, Dict, List, Pattern, Type
 
 from knot_resolver_manager.exceptions import SchemaException
 from knot_resolver_manager.utils import CustomValueType
@@ -57,6 +57,32 @@ class StrBase(CustomValueType):
     @classmethod
     def json_schema(cls: Type["StrBase"]) -> Dict[Any, Any]:
         return {"type": "string"}
+
+
+class EscStrBase(StrBase):
+    r"""
+    Base class to escape some chars.
+    Just inherit the class and set escaped characters in '_esc'.
+
+    class EscTabStr(PatternBase):
+        _esc: List[str] = ["\t"]
+    """
+
+    _esc_chars: List[str]
+
+    def __init__(self, source_value: Any, object_path: str = "/") -> None:
+        super().__init__(source_value, object_path)
+        if isinstance(source_value, (str, int)) and not isinstance(source_value, bool):
+            source_str = str(source_value)
+            for esc_char in self._esc_chars:
+                source_str = source_str.replace(esc_char, rf"\{esc_char}")
+            self._value = source_str
+        else:
+            raise SchemaException(
+                f"Unexpected value for '{type(self)}'."
+                f" Expected string or int, got '{source_value}' with type '{type(source_value)}'",
+                object_path,
+            )
 
 
 class IntRangeBase(IntBase):

facboy/knot-resolver

manager/knot_resolver_manager/datamodel/types/base_types.py
@@ -1,7 +1,8 @@
 import re
-from typing import Any, Dict, Pattern, Type
+from typing import Any, Dict, List, Pattern, Type
 
 from knot_resolver_manager.utils.modeling import BaseValueType
+from knot_resolver_manager.utils.modeling.exceptions import DataValidationError
 
 
 class IntBase(BaseValueType):
@@ -56,6 +57,32 @@ class StrBase(BaseValueType):
     @classmethod
     def json_schema(cls: Type["StrBase"]) -> Dict[Any, Any]:
         return {"type": "string"}
+
+
+class EscStrBase(StrBase):
+    r"""
+    Base class to escape some chars.
+    Just inherit the class and set escaped characters in '_esc'.
+
+    class EscTabStr(PatternBase):
+        _esc: List[str] = ["\t"]
+    """
+
+    _esc_chars: List[str]
+
+    def __init__(self, source_value: Any, object_path: str = "/") -> None:
+        super().__init__(source_value, object_path)
+        if isinstance(source_value, (str, int)) and not isinstance(source_value, bool):
+            source_str = str(source_value)
+            for esc_char in self._esc_chars:
+                source_str = source_str.replace(esc_char, rf"\{esc_char}")
+            self._value = source_str
+        else:
+            raise DataValidationError(
+                f"Unexpected value for '{type(self)}'."
+                f" Expected string or int, got '{source_value}' with type '{type(source_value)}'",
+                object_path,
+            )
 
 
 class IntRangeBase(IntBase):